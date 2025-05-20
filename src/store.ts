b74b7485ccd917e0fab87877def97aa70d0910bb
Lucas <gorgulho.lucas0@gmail.com>
2025-05-20 07:40:20 -0300

    botao de add e remover do carrinho

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ import Product from "./app/loading";
 type CartState = {
   cart: ProductType[];
   addProduct: (product: ProductType) => void;
-  //removeProduct: (productId: string) => void;
+  removeProduct: (product: ProductType) => void;
   isOpen: boolean;
   toggleCart: () => void;
 };
@@ -19,7 +19,6 @@ export const useCartStore = create<CartState>()(
       addProduct: (item) => 
         set((state) => {
           const product = state.cart.find((p) => p.id === item.id);
-
           if (product) {
             const updatedCart = state.cart.map((p) => {
               if(p.id === item.id) {
@@ -27,14 +26,33 @@ export const useCartStore = create<CartState>()(
               } 
               return p;
           });
-
           return { cart:updatedCart };
-
           }else{
             return { cart: [...state.cart, { ...item, quantity: 1 }]}
           } 
-          
       }),
+
+
+
+      removeProduct: (item) => 
+        set((state) => {
+          const existingProduct = state.cart.find((p) => p.id === item.id);
+          
+          if (existingProduct && existingProduct.quantity! > 1) {
+            const updatedCart = state.cart.map((p) => {
+              if(p.id === item.id) {
+                return { ...p, quantity: p.quantity! -1};
+              } 
+              return p;
+            });
+              return {cart: updatedCart};
+          } else {
+            const filterdCart = state.cart.filter((p) => p.id !== item.id);
+            return {cart: filterdCart};
+          }
+      }),
+
+
 
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
